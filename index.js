71801b716911b39813667fa4b6fa2ac7e06ec172
osmomysl <dannvx@gmail.com>
2023-03-14 23:27:35 +0200

    index, groupByRatio: write groups into a single file

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 import fs from 'fs/promises'
-import {groupByRatio} from './src/process-sequences.js'
+import {groupByRatio as _groupByRatio} from './src/process-sequences.js'
 
 /**
  * @param {String} sequencesPath relative path to a JSON-formatted file containing sequences
- * @param {String} groupPrefix relative pathname including filename prefix for a group. Group's ratio will be appended
- * @param {String} groupSuffix filename suffix for a group. This will be appended to the filename of a group. E.g., an extension.
- * @description read given file, groups the sequences in it into groups and writes each group into a separate file
+ * @param {String} groupedPath relative pathname to the output file
+ * @description reads given file, groups the sequences in it into groups and writes those in JSON to a file
 */
-async function groupByRatio(sequencesPath, groupPrefix, groupSuffix) {
+async function groupByRatio(sequencesPath, groupedPath) {
     const sequences = JSON.parse(await fs.readFile(sequencesPath, 'utf8'))
 
-    const groups = groupByRatio(sequences)
+    const groups = _groupByRatio(sequences)
 
-    for (const [i, group] of groups.entries()) {
-        await fs.writeFile(`${groupPrefix}${i}${groupSuffix}`, JSON.stringify(group, null, 2))
-    }
+    await fs.writeFile(groupedPath, JSON.stringify(groups, null, 2))
 }
 
-async function groupByRatio18() {
-    await groupByRatio('sequences18.json', 'sequences18-group', '.json')
-}
-
-export {groupByRatio, groupByRatio18}
+export {groupByRatio}
